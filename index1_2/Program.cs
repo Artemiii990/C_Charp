54ff81a49c74db710ac5a5dbc156b3c8e1b20afc
Artemi <133803304+Artemiii990@users.noreply.github.com>
2024-12-03 19:00:18 +0200

    New dir and files of C_Charp added

---
+++ index1_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace lesson1_2
+
+{
+    public class index1_2
+    {
+        public static void Main(string[] args)
+        {
+            byte by = 255; //8 біт 000
+            short num1 = 15;
+            int number = 25;
+            long num2 = 35;
+
+            float num3 = 45.44f; //32
+            double num4 = 45.44; //65
+            decimal money = 45.6m; //128
+
+            char letter = 'a';
+
+            bool flag = false;
+            string str1 = "Hello";
+            object obj1 = 45.5;
+            obj1 = "Hello";
+            
+            
+            var str2 = "Hello";
+
+
+            Console.WriteLine("My obj = " + str2);
+            Console.Write("Enter new obj = ");
+            string new_obj = Console.ReadLine();
+            Console.WriteLine("File = " + new_obj);
+        }
+    }
+}
+
+class Student
+{
+    public int age;
+    public string name;
+
+}
+
+{
+        Student student = new Student();
+        student.age = 25;
+        student.name = "James";
+}
+    
---
+++ lesson_file3_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace lesson3_4 
+{
+    public class lesson_file3_4
+    {
+        public static void Main(string[] args)
+        {
+            string str = "Hello World!";
+            char[] arr = str.ToCharArray();
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item + "");
+            }
+        }
+    }
+}
