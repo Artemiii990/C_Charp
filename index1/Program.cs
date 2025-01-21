1bda1ab515542209398c4aae3fd6a25e37e11e4d
Artemi <133803304+Artemiii990@users.noreply.github.com>
2025-01-21 16:26:22 +0200

    New files of home_task_10_11 added

---
+++ index1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace home_task_10_11
+
+{
+    public class index1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение из цифр(0-9):");
+            string inputString = Console.ReadLine();
+
+            try
+            {
+                int convertedNumber = ConvertStringToInt(inputString);
+                Console.WriteLine("Переделанное число под(int): " + convertedNumber);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Число вышло за диапазон.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Некорректный формат ввода данных.");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Введений рядок пустий.");
+            }
+
+        }
+        
+        public static int ConvertStringToInt(string inputString)
+        {
+            try
+            {
+                return int.Parse(inputString);
+            }
+            catch (OverflowException ex)
+            {
+
+                throw new OverflowException("Переполнение чисел", ex);
+            }
+            catch(FormatException ex)
+            {
+                throw new FormatException("Неверный формат", ex);
+            }
+            catch(ArgumentNullException ex)
+            {
+                throw new ArgumentNullException("Рядок не может быть пустым", ex);
+            }
+        }
+    }
+}
+
---
+++ index2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace home_task_10_11
+
+{
+    public class index2
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку из 0 и 1:");
+            string binaryString = Console.ReadLine();
+
+            try
+            {
+                int decimalNumber = ConvertBinaryToDecimal(binaryString);
+                Console.WriteLine("Преобразованное число: " + decimalNumber);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Число выходит за пределы диапазона типа int.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Некорректный формат введенной строки (не двоичное число).");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Введенная строка пустая.");
+            }
+        }
+
+        public static int ConvertBinaryToDecimal(string binaryString)
+        {
+            if (string.IsNullOrEmpty(binaryString))
+            {
+                throw new ArgumentNullException("Введенная строка пустая.");
+            }
+
+            int decimalNumber = 0;
+            try
+            {
+                checked
+                {
+                    for (int i = 0; i < binaryString.Length; i++)
+                    {
+                        char c = binaryString[i];
+                        if (c != '0' && c != '1')
+                        {
+                            throw new FormatException("Некорректный формат введенной строки (не двоичное число).");
+                        }
+
+                        decimalNumber = decimalNumber * 2 + (c - '0');
+                    }
+                }
+            }
+            catch (OverflowException ex)
+            {
+                throw new OverflowException("Переполнение int",
+                    ex); 
+            }
+
+            return decimalNumber;
+        }
+    }
+}
