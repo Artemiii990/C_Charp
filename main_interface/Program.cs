9d002cba2c4490dbe40bff4099012bd14e01614a
Artemi <133803304+Artemiii990@users.noreply.github.com>
2025-01-28 13:28:58 +0200

    interface_files of work added

---
+++ main_interface/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace main_interface
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ICalc lessCalc = new LessCalc();
+            lessCalc.Less(15); 
+
+
+            ICalc greaterCalc = new GreaterCalc();
+            greaterCalc.Greater(5); 
+
+        }
+    }
+
+
+    interface ICalc
+    {
+        void Less(int valueToCompare);
+        void Greater(int valueToCompare);
+    }
+
+    class LessCalc : ICalc
+    {
+        public void Less(int valueToCompare)
+        {
+            for (int i = 0; i < valueToCompare; i++)
+            {
+                Console.WriteLine("{0} is less than {1}", i, valueToCompare); 
+            }
+        }
+
+
+        public void Greater(int valueToCompare)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class GreaterCalc : ICalc
+    {
+        public void Greater(int valueToCompare)
+        {
+            for (int i = valueToCompare + 1; i <= valueToCompare + 10; i++) 
+            {
+                Console.WriteLine("{0} is greater than {1}", i, valueToCompare); 
+            }
+        }
+        public void Less(int valueToCompare)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+
+
+}
---
+++ main_interface_2/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace main_interface_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Imath minProg = new Min_programm();
+            minProg.min();
+
+            Imath maxProg = new Max_programm();
+            maxProg.max(); 
+
+            Imath avgProg = new Average_programm();
+            avgProg.average();
+
+            Imath searchProg = new Search_programm();
+            searchProg.Search(5);
+
+
+        }
+    }
+
+    interface Imath
+    {
+        void min();
+        void max();
+        void average();
+        void Search(int valueToSearch);
+    }
+
+    class Min_programm : Imath
+    {
+        public void min()
+        {
+            int minVal = 1;
+            for (int i = 2; i < 10; i++)
+            {
+                if (i < minVal)
+                {
+                    minVal = i;
+                }
+            }
+            Console.WriteLine($"Minimum value: {minVal}");
+
+        }
+
+        public void max()
+        {
+             throw new NotImplementedException();
+        }
+
+        public void average()
+        {
+             throw new NotImplementedException();
+        }
+
+        public void Search(int valueToSearch)
+        {
+             throw new NotImplementedException();
+        }
+    }
+
+    class Max_programm : Imath
+    {
+        public void max()
+        {
+            int maxVal = 1;
+            for (int i = 2; i < 10; i++)
+            {
+                if (i > maxVal)
+                {
+                    maxVal = i;
+                }
+            }
+            Console.WriteLine($"Maximum value: {maxVal}");
+        }
+
+        public void min()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void average()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Search(int valueToSearch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Average_programm : Imath
+    {
+        public void average()
+        {
+            int sum = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"Average value: {(double)sum / 9}");
+
+        }
+
+        public void min()
+        {
+             throw new NotImplementedException();
+        }
+
+        public void max()
+        {
+             throw new NotImplementedException();
+        }
+
+        public void Search(int valueToSearch)
+        {
+             throw new NotImplementedException();
+        }
+    }
+
+    class Search_programm : Imath
+    {
+        private int entervalueToSearch;
+
+        public void Search(int valueToSearch)
+        {
+            Console.WriteLine("Enter the value to search: ");
+            entervalueToSearch = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Values equal (Equals)? {entervalueToSearch.Equals(valueToSearch)}");
+        }
+
+        public void min()
+        {
+             throw new NotImplementedException();
+        }
+
+        public void max()
+        {
+             throw new NotImplementedException();
+        }
+
+        public void average()
+        {
+             throw new NotImplementedException();
+        }
+    }
+}
