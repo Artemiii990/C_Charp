9f3e9037763a488dbe881c52af60fcffb705b294
Artemi <133803304+Artemiii990@users.noreply.github.com>
2025-01-28 11:42:01 +0200

    Figure abstract class added

---
+++ abstract_file/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace abstract_file
+
+{
+    public class Square
+    {
+        public static void Main(string[] args)
+        {
+            Figure rectangle = new rectangle();
+            rectangle.count_squares();
+            Figure circle = new circle();
+            circle.count_squares();
+            Figure triangle = new triangle();
+            triangle.count_squares();
+            Figure trapezium = new trapezium();
+            trapezium.count_squares();
+        }
+    }
+
+    abstract class Figure
+    {
+        public abstract void count_squares();
+    }
+
+
+    class rectangle:Figure
+    {
+        public override void count_squares()
+        {
+            Console.WriteLine("square of rectangle = a*b");
+        }
+        
+    }
+    
+    class circle:Figure
+    {
+        public override void count_squares()
+        {
+            Console.WriteLine("square of circle = Пr2");
+        }
+        
+    }
+    
+    class triangle:Figure
+    {
+        public override void count_squares()
+        {
+            Console.WriteLine("square of triangle = (a · h)/2");
+        }
+        
+    }
+    
+    class trapezium:Figure
+    {
+        public override void count_squares()
+        {
+            Console.WriteLine("square of trapezium = 1/2(a + b)h");
+        }
+        
+    }
+}
