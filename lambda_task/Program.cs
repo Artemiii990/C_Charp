e8f2194be10df57d090a2b9465ac017904d5ad3c
Artemi <133803304+Artemiii990@users.noreply.github.com>
2025-01-29 20:42:15 +0200

    lambda_file and backpack added to git by ARTEMKAA)

---
+++ lambda_task/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+
+
+namespace lambda_task
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = { 7, 14, 21, 28, 35, 42, 10, 15, 20, 25, 50, 60, 77 };
+
+           
+            Func<int[], int, int> countMultiples = (arr, divisor) => arr.Count(num => num % divisor == 0);
+            
+            Console.WriteLine($"Количество чисел, кратных 7: {countMultiples(numbers, 7)}"); 
+            numbers
+                .Where(num => num % 7 == 0)
+                .ToList()
+                .ForEach(item => Console.WriteLine(" " + item));
+            Console.WriteLine($"Количество чисел, кратных 5: {countMultiples(numbers, 5)}"); 
+            Console.WriteLine($"Количество чисел, кратных 3: {countMultiples(numbers, 3)}"); 
+        }
+    }
+}
