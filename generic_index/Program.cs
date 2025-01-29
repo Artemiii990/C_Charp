7a3ae4ff426bad00e45d5675014ec262ade004ee
Artemi <133803304+Artemiii990@users.noreply.github.com>
2025-01-29 22:36:07 +0200

    generic code commit

---
+++ generic_index/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace generic_index1
+{
+    class Program
+    {
+        static T MaxItem<T>(T a, T b, T c) where T : IComparable<T>
+        {
+            T max = a;
+            if (b.CompareTo(max) > 0) max = b;
+            if (c.CompareTo(max) > 0) max = c;
+            return max;
+        }
+        
+        static T MinItem<T>(T a, T b, T c) where T : IComparable<T>
+        {
+            T min = a;
+            if (b.CompareTo(min) < 0) min = b;
+            if (c.CompareTo(min) < 0) min = c;
+            return min;
+        }
+
+        
+        static void Sum_items<T>(IEnumerable<T> coll)
+        {
+            dynamic sum = 0;
+            foreach (var item in coll)
+            {
+                sum += item;
+            }
+            Console.WriteLine("Сумма всех чисел массива:" + sum);
+        }
+
+        
+
+        public static void Main(string[] args)
+        {
+            int a = 45;
+            int b = 55;
+            int c = 30;
+            
+            int maxInt = MaxItem(a, b, c);
+            Console.WriteLine($"Максимальное целое число: {maxInt}"); 
+
+            int minInt = MinItem(a, b, c);
+            Console.WriteLine($"Минимальное целое число: {minInt}"); 
+
+            int[] arr = [40, 71, 12, 73, 47];
+            
+            Sum_items<int>(arr);
+
+        }
+    }
+}
