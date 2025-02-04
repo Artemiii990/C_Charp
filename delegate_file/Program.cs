0e11e4c390779b5eac693aa798b30cee5f0204da
Artemi <133803304+Artemiii990@users.noreply.github.com>
2025-02-04 18:45:12 +0200

    events was added to file delegate_file

--- delegate_file/Program.cs
+++ delegate_file/Program.cs
@@ -15,6 +15,11 @@ public class CreditCard
     public string Pin { get; private set; }
     public decimal CreditLimit { get; private set; }
     public decimal Balance { get; private set; }
+    
+    
+    public event Deposit OnDeposit;
+    public event Spend OnSpend;
+    public event Changepin OnChangePin;
 
     
     public CreditCard(string cardNumber, string ownerName, string expiryDate, string pin, decimal creditLimit)
@@ -27,12 +32,14 @@ public class CreditCard
         Balance = 150;
     }
     
+    
     public void Deposit(decimal amount)
     {
         if (amount > 0)
         {
             Balance += amount;
             Console.WriteLine($"Счет пополнено на {amount}. Поточный баланс: {Balance}.");
+            OnDeposit?.Invoke(amount);
         }
         else
         {
@@ -48,6 +55,7 @@ public class CreditCard
             {
                 Balance -= amount;
                 Console.WriteLine($"Потрачено {amount}. Поточный баланс: {Balance}.");
+                OnSpend?.Invoke(amount);
             }
             else if (amount <= Balance + CreditLimit)
             {
@@ -72,13 +80,18 @@ public class CreditCard
         {
             Pin = newPin;
             Console.WriteLine("PIN-код успешно изменен.");
+            OnChangePin?.Invoke(oldPin, newPin);
         }
         else
         {
             Console.WriteLine("Неверный PIN-код.");
         }
     }
-
+    
+    public void ProcessEvent(string eventType, string message) {
+        Console.WriteLine($"{eventType}: {message}");
+    }
+    
     public void CheckBalance()
     {
         Console.WriteLine("На баллансе ---- " + Balance + "грн");
@@ -92,6 +105,11 @@ class Program
     {
         CreditCard myCard = new CreditCard("1234 5678 9012 3456", "Иван Олександрович", "12/25", "5241", 1000);
         
+        myCard.OnDeposit += (amount) => myCard.ProcessEvent("Deposit", $"Засчитано на карту {amount} грн.");
+        myCard.OnSpend += (amount) => myCard.ProcessEvent("Spend", $"Снято с карты {amount} грн.");
+        myCard.OnChangePin += (oldPin, newPin) => myCard.ProcessEvent("ChangePin", $"PIN изменено с {oldPin} на {newPin}.");
+        
+        
         Deposit myDeposit = new Deposit(myCard.Deposit);
         myDeposit(650);
         
@@ -104,6 +122,8 @@ class Program
         myChangePin("5241", "3145");
         
         myCard.CheckBalance();
+        
+        
 
     }
 }
